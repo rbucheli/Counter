296e4c510b787de47e5e689192601b6872029dc8
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-06-22 23:39:03 -0400

    Select value and buttons

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Set initial count
 let count = 0; 
+
+// select value and buttons
+const value = document.querySelector("#value:");
+const btns = document.querySelector(".btn")
+
+console.log(btns);
